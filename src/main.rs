73d5258cb3aada7e9f19a908f451330622892c7f
HadsonRamalho <133451081+HadsonRamalho@users.noreply.github.com>
2023-11-28 19:15:55 -0300

    Adicionando funções para gerenciar e listar livros cadastrados
    (Também inicializa provisoriamente livros com campos básicos)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::io;
 use std::io::Read;
-use log::error;
 
 struct Biblioteca{
     titulo:String,
@@ -16,6 +15,43 @@ fn stoi(y:String) -> i32{ // Transformando uma string numérica em i32
     return y
 }
 
+fn mgl_listar(){
+    let mut livro : Vec<Biblioteca> = Vec::new();
+
+    let mut i = 0;
+    let itos = i.to_string();
+    while i < 3{
+        i += 1;
+        let itos = i.to_string();
+        let t = "Titulo";
+        let a = "Autor";
+        let concT = format!("{} {}", t, itos);
+        let concA = format!("{} {}", a, itos);
+        livro.push(Biblioteca {
+            titulo: String::from(concT),
+            id: i,
+            autor: String::from(concA),
+        });
+        //println!("Titulo: {}\nID: {}\nAutor: {}\n", livro.titulo, livro.id, livro.autor);
+    }
+    for livro in &livro{
+        println!("Titulo: {}\nID: {}\nAutor: {}", livro.titulo, livro.id, livro.autor);
+    }
+}
+
+fn menu_gerencia_livros(){
+    println!("1 - Listar todos os livros");
+    let mut opc = Default::default();
+    io::stdin()
+        .read_line(&mut opc)
+        .expect("Erro ao ler a opção");
+    let opc = stoi(opc);
+    match opc{
+        1 => mgl_listar(),
+        _ => println!("Opção inválida")
+    }
+}
+
 fn menu(){
     println!(" Menu Principal\n1 - Buscar livro\n2 - Gerenciar livros");
     let mut opc = Default::default();
@@ -27,6 +63,7 @@ fn menu(){
 
     match opc{
         1 => println!("ok"),
+        2 => menu_gerencia_livros(),
         _ => println!("Opção inválida")
     }
 }
@@ -42,24 +79,5 @@ fn menu(){
 */
 
 fn main() {
-    let mut livro : Vec<Biblioteca> = Vec::new();
-
-    let mut i = 0;
-    let itos = i.to_string();
-    while i < 3{
-        i += 1;
-        let itos = i.to_string();
-        let t = "Titulo";
-        let conc = format!("{}{}", t, itos);
-        livro.push(Biblioteca {
-            titulo: String::from(conc),
-            id: i,
-            autor: String::from("Autor{n}"),
-        });
-        //println!("Titulo: {}\nID: {}\nAutor: {}\n", livro.titulo, livro.id, livro.autor);
-    }
-    for livro in &livro{
-        println!("Titulo: {}\nID:{}\nAutor: {}", livro.titulo, livro.id, livro.autor);
-    }
     menu();
 }
