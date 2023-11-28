4409ba20841360dcc942c11c687dd16f6cb3e05c
HadsonRamalho <133451081+HadsonRamalho@users.noreply.github.com>
2023-11-28 20:18:52 -0300

    Salvando para correção

--- src/main.rs
+++ src/main.rs
@@ -15,63 +15,89 @@ fn stoi(y:String) -> i32{ // Transformando uma string numérica em i32
     return y
 }
 
-fn mgl_listar(){
-    let mut livro : Vec<Biblioteca> = Vec::new();
+fn mgl_listar(mut livro:Vec<Biblioteca>, T:u32){
 
-    let mut i = 0;
+    /*let mut i = 0;
     let itos = i.to_string();
     while i < 3{
         i += 1;
-        let itos = i.to_string();
+        let _itos = i.to_string();
         let t = "Titulo";
-        let a = "Autor";
-        let concT = format!("{} {}", t, itos);
-        let concA = format!("{} {}", a, itos);
+        let conc = format!("{}{}", t, itos);
         livro.push(Biblioteca {
-            titulo: String::from(concT),
+            titulo: String::from(conc),
             id: i,
-            autor: String::from(concA),
+            autor: String::from("Autor{n}"),
         });
         //println!("Titulo: {}\nID: {}\nAutor: {}\n", livro.titulo, livro.id, livro.autor);
-    }
+    }*/
+    let mut i = 0;
     for livro in &livro{
-        println!("Titulo: {}\nID: {}\nAutor: {}", livro.titulo, livro.id, livro.autor);
+        if i < T{
+            i += 1;
+            println!("Titulo: {}\nID:{}\nAutor: {}", livro.titulo, livro.id, livro.autor);
+        }
     }
 }
 
+fn mgl_adicionar(mut livro:Vec<Biblioteca>, mut T:u32){
+    println!("\tCadastrando novo livro!");
+    println!("Digite o titulo do livro: ");
+    let mut titulo = Default::default();
+    io::stdin()
+        .read_line(&mut titulo)
+        .expect("Erro ao ler o titulo do livro");
+    println!("Digite o autor do livro: ");
+    let mut autor = Default::default();
+    io::stdin()
+        .read_line(&mut autor)
+        .expect("Erro ao ler o autor do livro");
+    livro.push(Biblioteca {
+        titulo: titulo,
+        id: 1,
+        autor: autor,});
+    T += 1;
+}
+
 fn menu_gerencia_livros(){
+
+    let livro : Vec<Biblioteca> = Vec::new();
+    let T:u32 = 0;
+
     println!("1 - Listar todos os livros");
+    println!("2 - Adicionar um livro");
     let mut opc = Default::default();
     io::stdin()
         .read_line(&mut opc)
         .expect("Erro ao ler a opção");
     let opc = stoi(opc);
     match opc{
-        1 => mgl_listar(),
+        1 => mgl_listar(livro, T),
+        2 => mgl_adicionar(livro, T),
         _ => println!("Opção inválida")
     }
 }
 
 fn menu(){
-    println!(" Menu Principal\n1 - Buscar livro\n2 - Gerenciar livros");
+    println!(" Menu Principal\n1 - Buscar livro\n2 - Gerenciar livros\n3 - Sair");
     let mut opc = Default::default();
     io::stdin()
         .read_line(&mut opc)
         .expect("Erro ao obter a opção");
 
     let opc = stoi(opc);
-
-    match opc{
-        1 => println!("ok"),
-        2 => menu_gerencia_livros(),
-        _ => println!("Opção inválida")
+    while opc != 3{
+        match opc{
+            1 => println!("ok"),
+            2 => menu_gerencia_livros(),
+            3 => break,
+            _ => println!("Opção inválida")
+        }  
     }
+    
 }
 /*
-    livro.push(Biblioteca {
-        titulo: String::from("Livro1"),
-        id: 1,
-        autor: String::from("Autor1"),});
+
     livro.push(Biblioteca {
         titulo: String::from("Livro2"),
         id: 2,
