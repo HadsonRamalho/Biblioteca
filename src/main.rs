2a29947a055e9958c00b47b8a90923eb3d484577
Hadson <hadsonramalho@gmail.com>
2023-11-28 11:31:36 -0300

    Testando a manipulação de structs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::io;
 use std::io::Read;
 use log::error;
 
+struct Biblioteca{
+    titulo:String,
+    id:u32,
+    autor:String
+}
 
 fn stoi(y:String) -> i32{ // Transformando uma string numérica em i32
     let y: i32 = match y.trim().parse() {
@@ -25,6 +30,36 @@ fn menu(){
         _ => println!("Opção inválida")
     }
 }
+/*
+    livro.push(Biblioteca {
+        titulo: String::from("Livro1"),
+        id: 1,
+        autor: String::from("Autor1"),});
+    livro.push(Biblioteca {
+        titulo: String::from("Livro2"),
+        id: 2,
+        autor: String::from("Autor2")});
+*/
+
 fn main() {
+    let mut livro : Vec<Biblioteca> = Vec::new();
+
+    let mut i = 0;
+    let mut n = i.to_string();
+    while i < 3{
+        i = i + 1;
+        let n = i.to_string();
+        let t = "Titulo";
+        let conc = format!("{}{}", t, n);
+        livro.push(Biblioteca {
+            titulo: String::from(conc),
+            id: i,
+            autor: String::from("Autor{n}"),
+        });
+        //println!("Titulo: {}\nID: {}\nAutor: {}\n", livro.titulo, livro.id, livro.autor);
+    }
+    for livro in &livro{
+        println!("Titulo: {}\nID:{}\nAutor: {}", livro.titulo, livro.id, livro.autor);
+    }
     menu();
 }
